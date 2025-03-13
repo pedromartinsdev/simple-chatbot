c75e98a990ef216eebf938b19e4413a8013fa7a8
Pedro <pedrohenriquemartins@live.com>
2025-03-12 22:49:39 -0300

    update css

--- chatbot.js
+++ chatbot.js
@@ -1,29 +1,53 @@
 const responses = {
-  "ola": "Olá! Como posso te ajudar?",
+  "oi": "Olá! Como posso te ajudar?",
   "como você está?": "Bem e você?",
   "bem também!": "Ótimo!",
   "tchau": "Tchau! Até mais!"
 };
 
-function getResponse(input){
-  input = input.toLowerCase()
-  return responses[input]
-}
-
-function sendMessage(){
-  let userInput = document.getElementById("type-ask")
-  if(userInput.value){
-    console.log(getResponse(userInput.value))
-    addLine(userInput.value)
+const userInput = document.getElementById("type-ask")
+const button = document.getElementById("submit-ask")
+userInput.addEventListener("input", function () {
+  if (userInput.value.trim() !== "") {
+    button.disabled = false
   } else {
-    alert("Digite algo")
+    button.disabled = true
+  }
+})
+
+userInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    sendMessage()
+  }
+})
+
+function getResponse(input) {
+  let ask = input.toLowerCase()
+  if (responses[ask]) {
+    return responses[ask]
+  } else {
+    return "Não entendi sua pergunta"
   }
 }
 
-function addLine(input){
+function sendMessage() {
+  addLine(userInput.value, "ask")
+  addLine(getResponse(userInput.value), "response")
+  userInput.value = ""
+  button.disabled = true
+  scrollDown()
+}
+
+function addLine(input, type) {
   let newLine = document.createElement("span")
+  newLine.className = type
   newLine.textContent = input
-  
+
   chat = document.getElementById("chat-body")
   chat.appendChild(newLine)
+}
+
+function scrollDown() {
+  let chat = document.getElementById("chat-body")
+  chat.scrollTop = chat.scrollHeight
 }
